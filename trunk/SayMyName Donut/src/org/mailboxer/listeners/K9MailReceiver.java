a41f0f7d67267349fc7f0d4270255ad48605e80e
marcin.bogdanski <marcin.bogdanski@98f268f8-8e36-11de-849c-55eda45439ce>
2010-03-07 17:03:55 +0000

    Added PreferenceDialog and K9MailListener

---
+++ trunk/SayMyName Donut/src/org/mailboxer/listeners/K9MailReceiver.java
@@ -0,0 +1,44 @@
+package org.mailboxer.listeners;
+
+import org.mailboxer.saymyname.QueryBuilder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class K9MailReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	
+			
+			Log.e("SMN", "START");
+			
+//			Log.e("com.fsck.k9.intent.extra.ACCOUNT", 
+//					intent.getStringExtra("com.fsck.k9.intent.extra.ACCOUNT"));
+//			Log.e("com.fsck.k9.intent.extra.FOLDER", 
+//					intent.getStringExtra("com.fsck.k9.intent.extra.FOLDER"));
+//			//Log.e("com.fsck.k9.intent.extra.SENT_DATE", 
+//			//		intent.get("com.fsck.k9.intent.extra.SENT_DATE"));
+//			Log.e("com.fsck.k9.intent.extra.FROM", 
+//					intent.getStringExtra("com.fsck.k9.intent.extra.FROM"));
+//			Log.e("com.fsck.k9.intent.extra.SUBJECT", 
+//					intent.getStringExtra("com.fsck.k9.intent.extra.SUBJECT"));
+
+
+			Intent serviceIntent = new Intent(context, QueryBuilder.class);
+			serviceIntent.putExtra(
+					QueryBuilder.COMMUNICATION_TYPE, QueryBuilder.COMMUNICATION_EMAIL);
+			serviceIntent.putExtra(
+					QueryBuilder.EMAIL, intent.getStringExtra(
+							"com.fsck.k9.intent.extra.FROM"));
+			serviceIntent.putExtra(
+					QueryBuilder.SUBJECT, intent.getStringExtra(
+							"com.fsck.k9.intent.extra.SUBJECT"));
+			context.startService(serviceIntent);
+		
+	}
+}
